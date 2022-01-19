d956e3b364291567c7a4519d8d69fdc7b215385b
Aisha <63489725+AishaA-cpu@users.noreply.github.com>
2022-01-18 22:21:39 -0600

    deletes comment in shipment.py

--- app/models/shipments.py
+++ app/models/shipments.py
@@ -4,5 +4,5 @@ class Shipment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     address = db.Column(db.String, nullable=False)
     created_date = db.Column(db.DateTime)
-    #inventory = db.relationship("Inventory", back_populates="shipments")
+    
     
